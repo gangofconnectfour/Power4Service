4b098d0b7666dc2138914e8ffce0d2449eb3f506
brumax27 <tit-max@live.fr>
2019-03-22 15:46:43 +0100

    passage en kotlin

---
+++ src/main/kotlin/com/gangofconnectfour/powerfourservice/PowerfourserviceApplication.kt
@@ -0,0 +1,11 @@
+package com.gangofconnectfour.powerfourservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PowerfourserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PowerfourserviceApplication>(*args)
+}
