c44f81700ed360ce66fcb723a442d61e5a8822bc
Barogthor <f.thery3103@gmail.com>
2019-07-21 22:01:42 +0200

    random hit ia

--- src/main/java/com/gangofconnectfour/powerfourservice/facade/GreetingController.java
+++ src/main/java/com/gangofconnectfour/powerfourservice/facade/GreetingController.java
@@ -10,15 +10,19 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
+import java.util.Random;
+
 @Controller
 public class GreetingController {
 
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public ResponseEntity greeting(String message) throws Exception {
+    public ResponseEntity greeting(String json) throws Exception {
+        Random rand = new Random();
+        System.out.println(json);
         Thread.sleep(1000); // simulated delay
-        return ResponseEntity.ok("Hello, " + HtmlUtils.htmlEscape(message) + "!");
+        return ResponseEntity.ok("{hit:"+rand.nextInt(7)+"}");
     }
 
 }
